ddbaf7a7a7926c5fb1c9826acc183caa994bc03e
NikoKVCS <kvcsgame@hotmail.com>
2019-07-04 10:36:24 +0800

    q

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"net"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "go_grpc_protobuf/customer"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement customer.CustomerServer.
+type server struct {
+	savedCustomers []*pb.CustomerRequest
+}
+
+// CreateCustomer creates a new Customer
+func (s *server) CreateCustomer(ctx context.Context, in *pb.CustomerRequest) (*pb.CustomerResponse, error) {
+	s.savedCustomers = append(s.savedCustomers, in)
+	return &pb.CustomerResponse{Id: in.Id, Success: true}, nil
+}
+
+// GetCustomers returns all customers by given filter
+func (s *server) GetCustomers(filter *pb.CustomerFilter, stream pb.Customer_GetCustomersServer) error {
+	for _, customer := range s.savedCustomers {
+		if filter.Keyword != "" {
+			if !strings.Contains(customer.Name, filter.Keyword) {
+				continue
+			}
+		}
+		if err := stream.Send(customer); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type testinterface interface {
+	Speak() string
+}
+
+type cat struct {
+}
+
+func (c *cat) Speak() string {
+	return "i am cat"
+}
+
+func main3() {
+	var a testinterface
+	a = &cat{}
+	a.Speak()
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// Creates a new gRPC server
+	s := grpc.NewServer()
+	pb.RegisterCustomerServer(s, &server{})
+	s.Serve(lis)
+
+}
